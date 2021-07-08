75a6098aa6302db1928909ffe81f7864499258e8
remvst <remvst@gmail.com>
2021-07-07 22:36:33 -0400

    Background

--- src/graphics.js
+++ src/graphics.js
@@ -16,7 +16,20 @@ createPattern = (width, height, render) => {
     return ctx.createPattern(canvas, 'repeat');
 };
 
-BACKGROUND_PATTERN = createPattern(100, 100, (ctx) => {
-    ctx.fillStyle = '#ccc';
-    ctx.fillRect(0, 0, 100, 50);
+BACKGROUND_PATTERN = createPattern(640, 400, (ctx) => {
+    ctx.fillStyle = '#c8caca';
+    ctx.fillRect(0, 0, 640, 400);
+
+    ctx.fillStyle = '#bbbcbc';
+    ctx.fillRect(100, 0, -10, 20);
+    ctx.fillRect(100, 0, 30, 400);
+
+    ctx.fillRect(200, 0, 20, 400);
+    ctx.fillRect(220, 200, 10, 20);
+
+    ctx.fillRect(300, 0, 40, 400);
+    ctx.fillRect(300, 300, -10, 30);
+
+    ctx.fillRect(400, 0, 20, 400);
+    ctx.fillRect(400, 150, -10, 10);
 })
